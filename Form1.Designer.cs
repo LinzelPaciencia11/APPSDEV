e9d75784f015b3e2778349575a0aaa9cd8a04d29
LinzelPaciencia11 <113317924+LinzelPaciencia11@users.noreply.github.com>
2024-02-04 03:10:44 +0800

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WinFormsApp1
+﻿namespace WinFormsApp2
 {
     partial class Form1
     {
@@ -28,152 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            OrderForm = new Label();
+            label1 = new Label();
             label2 = new Label();
             label3 = new Label();
-            txtform1 = new TextBox();
-            txtlrg = new TextBox();
-            txtsm = new TextBox();
-            txtmd = new TextBox();
-            button1 = new Button();
-            btnadd = new Button();
-            label1 = new Label();
             label4 = new Label();
             label5 = new Label();
+            button1 = new Button();
+            txtlength = new TextBox();
+            label6 = new Label();
             SuspendLayout();
-            // 
-            // OrderForm
-            // 
-            OrderForm.AutoSize = true;
-            OrderForm.Font = new Font("Impact", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            OrderForm.Location = new Point(286, 36);
-            OrderForm.Name = "OrderForm";
-            OrderForm.Size = new Size(196, 45);
-            OrderForm.TabIndex = 0;
-            OrderForm.Text = "Order Form";
-            OrderForm.Click += label1_Click_1;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Impact", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(104, 96);
-            label2.Name = "label2";
-            label2.Size = new Size(226, 26);
-            label2.TabIndex = 1;
-            label2.Text = "Number of bags ordered:";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(366, 292);
-            label3.Name = "label3";
-            label3.Size = new Size(116, 17);
-            label3.TabIndex = 2;
-            label3.Text = "Your total cost is:";
-            label3.Click += label3_Click;
-            // 
-            // txtform1
-            // 
-            txtform1.Location = new Point(403, 103);
-            txtform1.Name = "txtform1";
-            txtform1.Size = new Size(98, 23);
-            txtform1.TabIndex = 3;
-            // 
-            // txtlrg
-            // 
-            txtlrg.Location = new Point(404, 138);
-            txtlrg.Name = "txtlrg";
-            txtlrg.Size = new Size(97, 23);
-            txtlrg.TabIndex = 4;
-            txtlrg.TextChanged += txtlrg_TextChanged;
-            // 
-            // txtsm
-            // 
-            txtsm.Location = new Point(404, 196);
-            txtsm.Name = "txtsm";
-            txtsm.Size = new Size(97, 23);
-            txtsm.TabIndex = 5;
-            txtsm.TextChanged += txtsm_TextChanged;
-            // 
-            // txtmd
-            // 
-            txtmd.Location = new Point(404, 167);
-            txtmd.Name = "txtmd";
-            txtmd.Size = new Size(97, 23);
-            txtmd.TabIndex = 6;
-            txtmd.TextChanged += txtmd_TextChanged;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(0, 0);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 7;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // btnadd
-            // 
-            btnadd.Font = new Font("Impact", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnadd.Location = new Point(403, 234);
-            btnadd.Name = "btnadd";
-            btnadd.Size = new Size(112, 46);
-            btnadd.TabIndex = 8;
-            btnadd.Text = "Place order";
-            btnadd.UseVisualStyleBackColor = true;
-            btnadd.Click += button2_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(271, 168);
+            label1.Font = new Font("Impact", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.DarkGreen;
+            label1.Location = new Point(297, 23);
             label1.Name = "label1";
-            label1.Size = new Size(68, 17);
-            label1.TabIndex = 9;
-            label1.Text = "Medium -";
-            label1.Click += label1_Click_2;
+            label1.Size = new Size(193, 43);
+            label1.TabIndex = 0;
+            label1.Text = "CONVERSION";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.DarkOliveGreen;
+            label2.Location = new Point(222, 86);
+            label2.Name = "label2";
+            label2.Size = new Size(151, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Enter length (cm) :";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.DarkOliveGreen;
+            label3.Location = new Point(270, 132);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Yard/s :";
+            label3.Click += label3_Click;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(277, 139);
+            label4.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.DarkOliveGreen;
+            label4.Location = new Point(270, 173);
             label4.Name = "label4";
-            label4.Size = new Size(51, 17);
-            label4.TabIndex = 10;
-            label4.Text = "Large -";
+            label4.Size = new Size(93, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Feet/s (ft.) :";
             label4.Click += label4_Click;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(279, 197);
+            label5.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.DarkOliveGreen;
+            label5.Location = new Point(270, 214);
             label5.Name = "label5";
-            label5.Size = new Size(51, 17);
-            label5.TabIndex = 11;
-            label5.Text = "Small -";
+            label5.Size = new Size(69, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Inch/es :";
+            label5.Click += label5_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.FromArgb(0, 64, 0);
+            button1.Location = new Point(436, 251);
+            button1.Name = "button1";
+            button1.Size = new Size(98, 35);
+            button1.TabIndex = 5;
+            button1.Text = "Convert";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // txtlength
+            // 
+            txtlength.Location = new Point(427, 88);
+            txtlength.Name = "txtlength";
+            txtlength.Size = new Size(117, 23);
+            txtlength.TabIndex = 8;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.DarkOliveGreen;
+            label6.Location = new Point(451, 289);
+            label6.Name = "label6";
+            label6.Size = new Size(42, 20);
+            label6.TabIndex = 9;
+            label6.Text = "label";
+            label6.Click += label6_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label6);
+            Controls.Add(txtlength);
+            Controls.Add(button1);
             Controls.Add(label5);
             Controls.Add(label4);
-            Controls.Add(label1);
-            Controls.Add(btnadd);
-            Controls.Add(button1);
-            Controls.Add(txtmd);
-            Controls.Add(txtsm);
-            Controls.Add(txtlrg);
-            Controls.Add(txtform1);
             Controls.Add(label3);
             Controls.Add(label2);
-            Controls.Add(OrderForm);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -183,17 +152,13 @@
 
         #endregion
 
-        private Label OrderForm;
+        private Label label1;
         private Label label2;
         private Label label3;
-        private TextBox txtform1;
-        private TextBox txtlrg;
-        private TextBox txtsm;
-        private TextBox txtmd;
-        private Button button1;
-        private Button btnadd;
-        private Label label1;
         private Label label4;
         private Label label5;
+        private Button button1;
+        private TextBox txtlength;
+        private Label label6;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,22 +1,19 @@
-namespace WinFormsApp1
+namespace WinFormsApp2
 {
     public partial class Form1 : Form
     {
-        // Move these declarations to the class level to make them accessible throughout the class
-        int lrg = 2, md = 1, sm = 1;
-        double lp = 3.00, mp = 2.50, sp = 2.00;
-
+        int yards, remainingCm, feet, cInches;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void label1_Click_1(object sender, EventArgs e)
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
@@ -31,35 +28,28 @@ namespace WinFormsApp1
 
         }
 
-        private void label1_Click_2(object sender, EventArgs e)
+        private void label5_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            // Move the declaration of tCost here, and calculate it based on user input
-            double tCost = (52 * 5.50) + (lp * lrg) + (mp * md) + (sp * sm);
+            int cLength = Convert.ToInt32(txtlength.Text);
+            yards = cLength / 91;
+            remainingCm = cLength % 91;
+            feet = remainingCm / 30;
+            cInches = (int)((remainingCm % 30) / 2.54);
 
-            label3.Text = "Your total cost is: $" + tCost.ToString("N2");
+            label6.Text = yards + " yards, " + feet + " feet (foot), " + cInches + " inches";
         }
 
-        private void txtlrg_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtmd_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtsm_TextChanged(object sender, EventArgs e)
+        private void label6_Click(object sender, EventArgs e)
         {
 
         }
