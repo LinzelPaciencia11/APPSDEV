4a48835e5c95cdcb49fbb69945819d95db178b2c
LinzelPaciencia11 <113317924+LinzelPaciencia11@users.noreply.github.com>
2024-02-29 06:42:44 -0800

    Add files via upload

---
+++ practice3/practice3/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace practice3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbox1 = new System.Windows.Forms.TextBox();
+            this.txtbox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtbox3 = new System.Windows.Forms.TextBox();
+            this.btnbutton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(295, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ENTER NUMBER 1: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(295, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ENTER NUMBER 2: ";
+            // 
+            // txtbox1
+            // 
+            this.txtbox1.Location = new System.Drawing.Point(410, 126);
+            this.txtbox1.Name = "txtbox1";
+            this.txtbox1.Size = new System.Drawing.Size(100, 20);
+            this.txtbox1.TabIndex = 2;
+            // 
+            // txtbox2
+            // 
+            this.txtbox2.Location = new System.Drawing.Point(410, 177);
+            this.txtbox2.Name = "txtbox2";
+            this.txtbox2.Size = new System.Drawing.Size(100, 20);
+            this.txtbox2.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Controls.Add(this.txtbox3);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox1.Location = new System.Drawing.Point(298, 218);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(212, 110);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CHOICES";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(139, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "A- addition";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(124, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "B- subtraction";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(113, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "C - multiplication";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(140, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "D- division";
+            // 
+            // txtbox3
+            // 
+            this.txtbox3.BackColor = System.Drawing.SystemColors.Info;
+            this.txtbox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox3.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtbox3.Location = new System.Drawing.Point(6, 75);
+            this.txtbox3.Name = "txtbox3";
+            this.txtbox3.Size = new System.Drawing.Size(46, 26);
+            this.txtbox3.TabIndex = 9;
+            // 
+            // btnbutton
+            // 
+            this.btnbutton.Location = new System.Drawing.Point(380, 334);
+            this.btnbutton.Name = "btnbutton";
+            this.btnbutton.Size = new System.Drawing.Size(62, 27);
+            this.btnbutton.TabIndex = 5;
+            this.btnbutton.Text = "ENTER";
+            this.btnbutton.UseVisualStyleBackColor = true;
+            this.btnbutton.Click += new System.EventHandler(this.btnbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnbutton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtbox2);
+            this.Controls.Add(this.txtbox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbox1;
+        private System.Windows.Forms.TextBox txtbox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtbox3;
+        private System.Windows.Forms.Button btnbutton;
+    }
+}
+
---
+++ practice3/practice3/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnbutton_Click(object sender, EventArgs e)
+        {
+            double box1 = Convert.ToDouble(txtbox1.Text);
+            double box2 = Convert.ToDouble(txtbox2.Text);
+            char box3 = Convert.ToChar(txtbox3.Text);
+
+            double add = box1 + box2;
+            double sub = box1 - box2;
+            double mtp = box1 * box2;
+            double div = box1 / box2;
+
+            Form2 hi = new Form2();
+
+            switch (box3)
+            {
+                case 'A':
+                case 'a':
+                    hi.displayAdd(add);
+                    hi.Show();
+                    break;
+                case 'B':
+                case 'b':
+                    hi.displaySub(sub);
+                    hi.Show();
+                    break;
+                case 'C':
+                case 'c':
+                    hi.displayMtp(mtp);
+                    hi.Show();
+                    break;
+                case 'D':
+                case 'd':
+                    hi.displayDiv(div);
+                    hi.Show();
+                    break;
+            }
+
+        }
+    }
+}
